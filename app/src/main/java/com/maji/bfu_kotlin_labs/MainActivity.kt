31ea6ddd3d64709201694efb00f03eb21f8dc21a
Матвей Калашников <kmgpmain@gmail.com>
2025-03-09 23:04:03 +0200

    laba-2

--- app/src/main/java/com/maji/bfu_kotlin_labs/MainActivity.kt
+++ app/src/main/java/com/maji/bfu_kotlin_labs/MainActivity.kt
@@ -1,47 +1,60 @@
 package com.maji.bfu_kotlin_labs
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.maji.bfu_kotlin_labs.ui.theme.BFUKotlinLabsTheme
+import android.text.InputType
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            BFUKotlinLabsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_registration)
+
+        val usePhoneNumberTV = findViewById<TextView>(R.id.use_phone_number_tv)
+        val useEmailTV = findViewById<TextView>(R.id.use_email_tv)
+
+        val loginInput = findViewById<EditText>(R.id.register_login_input)
+        val passwordInput = findViewById<EditText>(R.id.register_password_input)
+        val repeatPasswordInput = findViewById<EditText>(R.id.repeat_password_input)
+
+        val registerButton = findViewById<Button>(R.id.register_button)
+
+        usePhoneNumberTV.setOnClickListener {
+            loginInput.hint = getString(R.string.enter_phone_number)
+            loginInput.inputType = InputType.TYPE_CLASS_PHONE
+            usePhoneNumberTV.setTextColor(ContextCompat.getColor(this, R.color.purple_500))
+            useEmailTV.setTextColor(ContextCompat.getColor(this, R.color.black))
+        }
+
+        useEmailTV.setOnClickListener {
+            loginInput.hint = getString(R.string.enter_email)
+            loginInput.inputType = InputType.TYPE_CLASS_TEXT
+            useEmailTV.setTextColor(ContextCompat.getColor(this, R.color.purple_500))
+            usePhoneNumberTV.setTextColor(ContextCompat.getColor(this, R.color.black))
+        }
+
+        registerButton.setOnClickListener {
+            val toast = Toast.makeText(this, "", Toast.LENGTH_LONG)
+
+            var message = ""
+            if (loginInput.inputType == InputType.TYPE_CLASS_TEXT && !loginInput.text.toString().contains("@")) {
+                message = "Введите корректную электронную почту."
+            } else if (loginInput.inputType == InputType.TYPE_CLASS_PHONE && !loginInput.text.toString().contains("+")) {
+                message = "Введите корректный номер телефона."
+            } else if (passwordInput.text.length < 8) {
+                message = "Минимальная длина пароля 8 символов."
+            } else if (passwordInput.text.equals(repeatPasswordInput.text)) {
+                message = "Введенные пароли разные."
+            }
+
+            if (message.isNotEmpty()) {
+                toast.setText(message)
+                toast.show()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BFUKotlinLabsTheme {
-        Greeting("Android")
     }
 }
